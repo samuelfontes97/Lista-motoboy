d41a019b8ad23d19ecc80af170c03568ca43aecd
samuelfontes97 <samuelfontes97@gmail.com>
2025-05-17 11:44:56 -0300

    lista-motoboy-1.1
    Feito alteração nop codigo para que fique responsivo a tela do smarphone
    -Alterçao para tete de download

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'entregas-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
