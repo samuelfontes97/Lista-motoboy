1ff96d3fe822801c0692449dfd3c3ee3ec87c029
samuelfontes97 <samuelfontes97@gmail.com>
2025-05-18 21:41:05 -0300

    motoboy-list
    versão 1.4

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const cacheName = 'entregas-cache-v2';
+const cacheName = 'entregas-cache-v3';
 const filesToCache = [
   './',
   './index.html',
+  './style.css',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './images/entregas.png',
+  './images/icon-192.png',
+  './images/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -13,10 +15,21 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== cacheName).map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
 
 
